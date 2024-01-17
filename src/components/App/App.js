968bb081bcc33d1b9244305449c3e655c006df2c
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-17 13:46:53 -0500

    deleted unused code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-// import { getUrls } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
